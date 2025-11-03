8a5feb74b9a3e33df92831cf708d2eb8aa7c1baf
Piyush Goenka <goenkapiyush2005@gmail.com>
2025-11-03 16:08:38 +0530

    fix deployment issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Footer />
-          {/* <SplashCursor /> */}
+          <SplashCursor />
           <Analytics />
         </ThemeProvider>
       </body>
